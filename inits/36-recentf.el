bc85a20ba3d4b5a186b435eae2de93b74df50d54
niku <niku@niku.name>
2014-03-24 15:52:42 +0900

    Save all of recentf items
    2000 ->
    all

--- inits/36-recentf.el
+++ inits/36-recentf.el
@@ -1,5 +1,5 @@
 (require 'recentf)
-(setq recentf-max-saved-items 2000)
+(setq recentf-max-saved-items nil) ; A nil value means to save the whole list.
 (setq recentf-exclude '(".recentf" "/TAGS$" "/var/tmp/"))
 (setq recentf-auto-cleanup 10)
 (run-with-idle-timer 30 t 'recentf-save-list)
