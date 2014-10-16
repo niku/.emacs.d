434b27f667933ef393eb170bc70dd272d9c101fd
niku <niku@niku.name>
2014-10-16 11:47:33 +0900

    Load color-theme without confirming

--- inits/35-color-theme.el
+++ inits/35-color-theme.el
@@ -1,1 +1,4 @@
-(load-theme 'solarized-theme)
+; The second argument "t" means "NO-CONFIRM".
+; It avoids confirming at starting emacs everytime.
+; see (describe-function 'load-theme)
+(load-theme 'solarized-dark t)
