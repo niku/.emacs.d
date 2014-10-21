8d80351cc58cb00534e3c949d4c906f0f74d5362
niku <niku@niku.name>
2014-10-22 00:16:55 +0900

    Use dictionary.app from emacs

---
+++ inits/cocoa-emacs-dictionary.el
@@ -0,0 +1,15 @@
+(defun dictionary-search (word)
+  (browse-url
+   (concat "dict:///" (url-hexify-string word))))
+
+(defun dictionary-word ()
+  (interactive)
+  (dictionary-search
+   (substring-no-properties (thing-at-point 'word))))
+
+(defun dictionary-region (beg end)
+  (interactive "r")
+  (dictionary-search
+   (buffer-substring-no-properties beg end)))
+
+(global-set-key (kbd "s-d") 'dictionary-word)
