7595341b09c952ebb03b01bf59570663e828ace4
niku <niku@niku.name>
2014-11-05 23:12:20 +0900

    Remove unused packages

--- inits/49-text-adjust.el
+++
@@ -1,3 +0,0 @@
-;; (auto-install-from-url "http://taiyaki.org/elisp/mell/src/mell.el")
-;; (auto-install-from-url "http://taiyaki.org/elisp/text-adjust/src/text-adjust.el")
-(require 'text-adjust)
--- inits/50-text-translator.el
+++
@@ -1,5 +0,0 @@
-;; (auto-install-batch "text translator")
-(require 'text-translator)
-(setq text-translator-auto-selection-func
-      'text-translator-translate-by-auto-selection-enja)
-(global-set-key (kbd "C-c M-t") 'text-translator-all-by-auto-selection)
