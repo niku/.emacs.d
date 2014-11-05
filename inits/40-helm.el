4ab42db7ac0ab706cb2f144f61dad016d6d0b1cc
niku <niku@niku.name>
2014-11-05 23:11:17 +0900

    Remove unused part

--- inits/40-helm.el
+++ inits/40-helm.el
@@ -1,5 +1,3 @@
-; (package-install 'helm)
-
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-;") 'helm-for-files)
@@ -7,33 +5,10 @@
 (global-set-key (kbd "M-%") 'helm-regexp)
 (global-set-key (kbd "<help> a") 'helm-apropos)
 
-;; (auto-install-batch "helm")
-;; (require 'helm-startup)
-;; (global-set-key (kbd "C-;") 'helm-filelist+)
-;; (global-set-key (kbd "C-o") 'helm-imenu)
-;; ; (browse-url "http://repo.or.cz/w/helm-config.git/blob_plain/HEAD:/contrib/make-filelist.rb")
-;; (setq helm-c-filelist-file-name "~/tmp/all.filelist")
+(eval-after-load "helm"
+  '(progn
+     (define-key helm-map (kbd "C-h") 'delete-backward-char)))
 
-;; ;; 過去の kill-ring の内容を取り出す
-;; (global-set-key (kbd "M-y") 'helm-show-kill-ring)
-
-;; ;; (auto-install-from-emacswiki "color-moccur.el")
-;; ;; (auto-install-from-emacswiki "moccur-edit.el")
-;; (require 'moccur-edit)
-;; (setq moccur-split-word t)
-;; ;; (auto-install-from-url "http://svn.coderepos.org/share/lang/elisp/helm-c-moccur/trunk/helm-c-moccur.el")
-;; (require 'helm-c-moccur)
-;; (global-set-key (kbd "M-o") 'helm-c-moccur-occur-by-moccur)
-;; (global-set-key (kbd "C-M-o") 'helm-c-moccur-dmoccur)
-;; (add-hook 'dired-mode-hook
-;;           '(lambda ()
-;;              (local-set-key (kbd "O") 'helm-c-moccur-dired-do-moccur-by-moccur)))
-
-;; ;; (require 'helm-rurima)
-;; ;; (setq helm-rurima-index-file "~/projects/rurima/rubydoc/rurima.e")
-
-;; (eval-after-load "helm"
-;;   '(define-key helm-map (kbd "C-h") 'delete-backward-char))
-
-
-;; ;: (auto-install-from-url "https://raw.github.com/gist/1982683/helm-git-project.el")
+(eval-after-load "helm-files"
+  '(progn
+     (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)))
