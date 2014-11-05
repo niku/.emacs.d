ff8b47245f96eb890226e0fffe02f0382c6c1e10
niku <niku@niku.name>
2014-11-05 23:07:15 +0900

    Update recentf configuration

--- inits/36-recentf.el
+++ inits/36-recentf.el
@@ -1,6 +1,5 @@
-(require 'recentf)
-(setq recentf-max-saved-items nil) ; A nil value means to save the whole list.
+(recentf-mode 1)
+(setq recentf-max-saved-items nil) ; save the whole list.
 (setq recentf-exclude '(".recentf" "/TAGS$" "/var/tmp/"))
 (setq recentf-auto-cleanup 10)
 (run-with-idle-timer 30 t 'recentf-save-list)
-(recentf-mode 1)
